ced3346c15717d6f18532b4b2bd0de94d7ec960c
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-01-06 21:36:35 -0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,19 +1,30 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
   const pricePerDay = 40;
-  let total = days * pricePerDay;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const shortTerm = 3;
+  const shortTermDiscount = 20;
 
-  if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+  const basePrice = days * pricePerDay;
+
+  if (days >= longTerm) {
+    return basePrice - longTermDiscount;
   }
 
-  return total;
+  if (days >= shortTerm) {
+    return basePrice - shortTermDiscount;
+  }
+
+  return basePrice;
+
 }
 
 module.exports = calculateRentalCost;
